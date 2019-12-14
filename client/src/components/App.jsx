e5f724f6b0b311051eef6623958bc6a5911c2ff3
David Kim <hyomithetech@gmail.com>
2019-12-14 10:28:53 -0800

    Refactored modal

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,120 +1,131 @@
-import React from 'react';
-import Carousel from './Carousel.jsx';
-import ProductDetail from './productDetail.jsx';
-import OptionButtons from './optionsButtons.jsx';
-import axios from 'axios'
-import AvailableOptions from './AvailableOptions.jsx';
-import Modal from './modal/modal.jsx';
-import ZoomPageModal from './modal/zoomPage.jsx';
+import React from "react";
+import Carousel from "./Carousel.jsx";
+import ProductDetail from "./productDetail.jsx";
+import OptionButtons from "./optionsButtons.jsx";
+import axios from "axios";
+import AvailableOptions from "./AvailableOptions.jsx";
+import ZoomPageModal from "./modal/zoomPage.jsx";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            list: null,
-            isOpen: false,
-            currentIndex: 0,
-            randomIndex: 0
-        }
-
-        this.zoomHandler = this.zoomHandler.bind(this)
-        this.indexGetter = this.indexGetter.bind(this)
-        this.showModal = this.showModal.bind(this)
-    }
-
-    componentDidMount() {
-        this.getProductData()
-    }
-
-    randomIndexGenerator(index) {
-        const random = Math.floor(Math.random() * index)
-
-        this.setState({
-            randomIndex: random
-        })
-    }
-
-    getProductData() {
-        axios.get('/ikea').then((response) => {
-            // console.log('hello from client', response.data)
-            this.setState({
-                list: response.data
-            })
-            this.randomIndexGenerator(response.data.length)
-        })
-    }
-
-    zoomHandler() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-
-    indexGetter(index) {
-        this.setState({
-            currentIndex: index
-        })
-    }
-
-    showModal(e) {
-        e.preventDefault()
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
+    this.state = {
+      list: null,
+      isOpen: false,
+      currentIndex: 0,
+      randomIndex: 0
     };
 
-    render() {
-        if (this.state.list === null) {
-            return null
-        } else {
-            return (
-                <div className="dk-theMainDiv" style={{
-                    fontFamily: `Noto Sans, sans-serif`
-                }}>
-                    <div className="dk-outerCategory">
-                        <ol className="dk-headerOl">
-                            <li className="dk-headerLi">
-                                Home
-                                </li>
-                            <span className="dk-headerSlash">/</span>
-                            <li className="dk-headerLi">
-                                Products
-                                    </li>
-                            <span className="dk-headerSlash">/</span>
-                            <li className="dk-headerLi">
-                                Furniture
-                                    </li>
-                            <span className="dk-headerSlash">/</span>
-                            <li className="dk-headerLi">
-                                Tables & desks
-                                    </li>
-                            <span className="dk-headerSlash">/</span>
-                            <li className="dk-headerLi">
-                                {this.state.list[this.state.randomIndex].name} {this.state.list[this.state.randomIndex].shortDesc.split(',')[0]}
-                            </li>
-                        </ol>
-                    </div>
-                    <div className="dk-carouselAndProductInfo">
-                        <Carousel randomIndex={this.state.randomIndex} indexGetter={this.indexGetter} zoom={this.zoomHandler} />
-                        <div className="dk-productDetail">
-                            <div>
-                                <ProductDetail data={this.state.list[this.state.randomIndex]} />
-                            </div><br />
-                            <div><OptionButtons data={this.state.list} /></div>
-                            <div><AvailableOptions /></div>
-                        </div>
-                    </div>
-                    <div>
-                        <Modal>
-                            <ZoomPageModal index={this.state.currentIndex} onClose={this.showModal} zoom={this.zoomHandler} imageSrc={this.state.list[this.state.randomIndex].imageSrc} show={this.state.isOpen} />
-                        </Modal>
-                    </div>
-                </div>
+    this.zoomHandler = this.zoomHandler.bind(this);
+    this.indexGetter = this.indexGetter.bind(this);
+    this.showModal = this.showModal.bind(this);
+  }
 
-            )
-        }
+  componentDidMount() {
+    this.getProductData();
+  }
+
+  randomIndexGenerator(index) {
+    const random = Math.floor(Math.random() * index);
+
+    this.setState({
+      randomIndex: random
+    });
+  }
+
+  getProductData() {
+    axios.get("/ikea").then(response => {
+      // console.log('hello from client', response.data)
+      this.setState({
+        list: response.data
+      });
+      this.randomIndexGenerator(response.data.length);
+    });
+  }
+
+  zoomHandler() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  indexGetter(index) {
+    this.setState({
+      currentIndex: index
+    });
+  }
+
+  showModal(e) {
+    e.preventDefault();
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    if (this.state.list === null) {
+      return null;
+    } else {
+      return (
+        <div
+          className="dk-theMainDiv"
+          style={{
+            fontFamily: `Noto Sans, sans-serif`
+          }}
+        >
+          <div className="dk-outerCategory">
+            <ol className="dk-headerOl">
+              <li className="dk-headerLi">Home</li>
+              <span className="dk-headerSlash">/</span>
+              <li className="dk-headerLi">Products</li>
+              <span className="dk-headerSlash">/</span>
+              <li className="dk-headerLi">Furniture</li>
+              <span className="dk-headerSlash">/</span>
+              <li className="dk-headerLi">Tables & desks</li>
+              <span className="dk-headerSlash">/</span>
+              <li className="dk-headerLi">
+                {this.state.list[this.state.randomIndex].name}{" "}
+                {
+                  this.state.list[this.state.randomIndex].shortDesc.split(
+                    ","
+                  )[0]
+                }
+              </li>
+            </ol>
+          </div>
+          <div className="dk-carouselAndProductInfo">
+            <Carousel
+              randomIndex={this.state.randomIndex}
+              indexGetter={this.indexGetter}
+              zoom={this.zoomHandler}
+            />
+            <div className="dk-productDetail">
+              <div>
+                <ProductDetail data={this.state.list[this.state.randomIndex]} />
+              </div>
+              <br />
+              <div>
+                <OptionButtons data={this.state.list} />
+              </div>
+              <div>
+                <AvailableOptions />
+              </div>
+            </div>
+          </div>
+          {this.state.isOpen ? (
+            <ZoomPageModal
+              index={this.state.currentIndex}
+              onClose={this.showModal}
+              zoom={this.zoomHandler}
+              imageSrc={this.state.list[this.state.randomIndex].imageSrc}
+              show={this.state.isOpen}
+            />
+          ) : null}
+        </div>
+      );
     }
+  }
 }
 
 export default App;
--- client/src/components/modal/zoomPage.jsx
+++ client/src/components/modal/zoomPage.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ZoomImages from '../zoomImages.jsx';
+import React from "react";
+import PropTypes from "prop-types";
+import ZoomImages from "../zoomImages.jsx";
+import Modal from "./modal.jsx";
 
-export default class ZoomPageModal extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        if (!this.props.show) {
-            return null;
-        }
-        return (
-            <div className="zoomImageOverFlow">
-                <button className="dk-zoomCloseButton" onClick={this.props.onClose}>
-                    <svg width="20" height="20">
-                        <path d="M17.597 5l-5.592 5.592L6.414 5 5 6.415l5.591 5.591L5 17.597l1.414 1.414 5.591-5.592 5.592 5.592 1.414-1.414-5.592-5.591 5.592-5.591z" />
-                    </svg>
-                </ button>
-                <div className="dk-zoomImage" style={{ 'transform': `translateY(-${(this.props.index * (100 / this.props.imageSrc.length) + 1)}%)` }}>
-
-                    {this.props.imageSrc.map((img) => (
-                        < ZoomImages list={img} key={img} />
-                    ))}
-                </div>
-            </div >
-        )
-    }
+export default function ZoomPageModal(props) {
+  return (
+    <Modal>
+      <div className="modal">
+        <div
+          style={{
+            position: "relative",
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          <div>
+            <button className="dk-zoomCloseButton" onClick={props.onClose}>
+              <svg width="20" height="20">
+                <path d="M17.597 5l-5.592 5.592L6.414 5 5 6.415l5.591 5.591L5 17.597l1.414 1.414 5.591-5.592 5.592 5.592 1.414-1.414-5.592-5.591 5.592-5.591z" />
+              </svg>
+            </button>
+            <div className="dk-zoomImage">
+              {props.imageSrc.map(img => (
+                <ZoomImages list={img} key={img} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
 }
 //
 
-
 ZoomPageModal.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    show: PropTypes.bool,
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool
 };
