0033bc486ca5f43fbacaf5ea6fec067ea0de5065
David Kim <hyomithetech@gmail.com>
2019-12-14 15:24:15 -0800

    Complete zoompage scrollable

--- client/src/components/modal/zoomPage.jsx
+++ client/src/components/modal/zoomPage.jsx
@@ -3,33 +3,58 @@ import PropTypes from "prop-types";
 import ZoomImages from "../zoomImages.jsx";
 import Modal from "./modal.jsx";
 
-export default function ZoomPageModal(props) {
-  return (
-    <Modal>
-      <div className="modal">
-        <div
-          style={{
-            position: "relative",
-            display: "flex",
-            justifyContent: "center"
-          }}
-        >
-          <div>
-            <button className="dk-zoomCloseButton" onClick={props.onClose}>
-              <svg width="20" height="20">
-                <path d="M17.597 5l-5.592 5.592L6.414 5 5 6.415l5.591 5.591L5 17.597l1.414 1.414 5.591-5.592 5.592 5.592 1.414-1.414-5.592-5.591 5.592-5.591z" />
-              </svg>
-            </button>
-            <div className="dk-zoomImage">
-              {props.imageSrc.map(img => (
-                <ZoomImages list={img} key={img} />
-              ))}
+export default class ZoomPageModal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.showThePicture(this.props.imageSrc[this.props.index]);
+    });
+  }
+
+  showThePicture(index) {
+    setTimeout(() => {
+      this.refs[index].scrollIntoView({ block: "center" });
+    });
+  }
+
+  render() {
+    // console.log(this.props.index);
+    // console.log(this.refs);
+    return (
+      <Modal>
+        <div className="modal">
+          <div
+            style={{
+              position: "relative",
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <div>
+              <button
+                className="dk-zoomCloseButton"
+                onClick={this.props.onClose}
+              >
+                <svg width="20" height="20">
+                  <path d="M17.597 5l-5.592 5.592L6.414 5 5 6.415l5.591 5.591L5 17.597l1.414 1.414 5.591-5.592 5.592 5.592 1.414-1.414-5.592-5.591 5.592-5.591z" />
+                </svg>
+              </button>
+              <div className="dk-zoomImage">
+                {this.props.imageSrc.map((img, i) => (
+                  <div ref={img}>
+                    <ZoomImages list={img} key={i} />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Modal>
-  );
+      </Modal>
+    );
+  }
 }
 //
 
