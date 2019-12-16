a240bcaa054740929792414544d9c7008a29c146
David Kim <hyomithetech@gmail.com>
2019-12-16 15:47:01 -0800

    Fix minor issue

--- client/src/components/MainSlide.jsx
+++ client/src/components/MainSlide.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
 
-const MainSlide = (props) => (
-
+const MainSlide = props => (
   <div className="dk-mainSlide">
     <img src={props.list} width="500" height="600" />
   </div>
-
-)
+);
 
 export default MainSlide;
