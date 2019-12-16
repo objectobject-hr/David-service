dc74dd1469c68b329151c0b69a18cf79e3d1f7b7
David Kim <hyomithetech@gmail.com>
2019-12-16 15:32:42 -0800

    Modify general css styling

--- client/src/components/optionsButtons.jsx
+++ client/src/components/optionsButtons.jsx
@@ -62,7 +62,7 @@ class OptionButtons extends React.Component {
               this.showBagModal(e);
             }}
           >
-            <span>
+            <span style={{ display: "flex", alignItems: "center" }}>
               <svg className="dk-shoppingBagIcon">
                 <path d="M20 15h2v2h-2v2h-2v-2h-2v-2h2v-2h2zm-6 2H6.441l-1.666-6h16.558L22 9h-5.323l-1.245-3.114L12.646 4h-1.292L8.569 5.886 7.322 9H2l2.544 8.633A2 2 0 0 0 6.441 19H14zM10.677 6h2.646l1.2 3H9.477z"></path>
               </svg>
@@ -77,7 +77,12 @@ class OptionButtons extends React.Component {
               this.showModal(e);
             }}
           >
-            <span>
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center"
+              }}
+            >
               <svg className="dk-shoppingListSvg">
                 <path d="M18,18h2v2H18v2H16V20H14V18h2V16h2ZM12,6H6V8h6ZM6,12H8V10H6ZM4,4H14l2-2H2V22H12V20H4ZM5.77,18.29l4.66-2.05L22,4.66,19.34,2,7.72,13.73Z"></path>
               </svg>
