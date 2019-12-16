cb0d7591c95e244aa4f168eae83cf91413e563d8
David Kim <hyomithetech@gmail.com>
2019-12-16 12:18:40 -0800

    Edit line height

--- client/src/components/productDetail.jsx
+++ client/src/components/productDetail.jsx
@@ -1,33 +1,27 @@
-import React from 'react';
-import StarReview from './starReview.jsx';
+import React from "react";
+import StarReview from "./starReview.jsx";
 
-const ProductDetail = (props) => (
+const ProductDetail = props => (
+  <div>
     <div>
-        <div>
-            <div className="dk-productDetailName">
-                {props.data.name}
-            </div>
-            <div>{props.data.shortDesc}</div>
-            <div className="dk-productPrice">${props.data.price}.00</div>
-            <div display="flex">
-                <span>
-                    <StarReview rating={props.data.rating} reviewNum={props.data.reviewNum} />
-                </span>
-                <span className="dk-reviewCount">
-                    {props.data.reviewNum} Reviews
-                </span>
-            </div>
-            <br />
-            <span >{props.data.midDesc}</span>
-            <span className="dk-readMore">
-                Read More
-            </span>
-        </div>
-        <div>
-
-        </div>
+      <div className="dk-productDetailName">{props.data.name}</div>
+      <div>{props.data.shortDesc}</div>
+      <div className="dk-productPrice">${props.data.price}.00</div>
+      <div display="flex">
+        <span>
+          <StarReview
+            rating={props.data.rating}
+            reviewNum={props.data.reviewNum}
+          />
+        </span>
+        <span className="dk-reviewCount">{props.data.reviewNum} Reviews</span>
+      </div>
+      <br />
+      <span style={{ lineHeight: 1.4 }}>{props.data.midDesc}</span>
+      <span className="dk-readMore">Read More</span>
     </div>
-
-)
+    <div></div>
+  </div>
+);
 
 export default ProductDetail;
